3cdb31938fdcd5e9734b035e72d5dd902e3a6830
ZiYun Lan <lanzeuen52011@gmail.com>
2023-03-20 17:23:44 +0800

    silder.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,68 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+
+// image slice
+
+const nextEl = document.querySelector(".next");
+
+const prevEl = document.querySelector(".prev");
+
+const imgsEl = document.querySelectorAll(".image_slider");
+
+const imageContainerEl = document.querySelector(".image-container");
+
+let currentImg = 1;
+
+let timeout;
+
+nextEl.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if (window.screen.availWidth > 1024){
+    if (currentImg > imgsEl.length) {
+      currentImg = 1;
+    } else if (currentImg < 1) {
+      currentImg = imgsEl.length;
+    }
+    imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 800}px)`;
+    timeout = setTimeout(() => {
+      currentImg++;
+      updateImg();
+    }, 3000);
+  }else{
+    if (currentImg > imgsEl.length) {
+      currentImg = 1;
+    } else if (currentImg < 1) {
+      currentImg = imgsEl.length;
+    }
+    imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 300}px)`;
+    timeout = setTimeout(() => {
+      currentImg++;
+      updateImg();
+    }, 3000);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
