b83e6f9ab1e56dcc10b91fd01be6502d49fd4869
ZiYun Lan <lanzeuen52011@gmail.com>
2023-03-23 11:15:15 +0800

    silder change.優化silder變成swiper

--- js/main.js
+++ js/main.js
@@ -8,71 +8,87 @@ collapsibles.forEach((item) =>
 
 // image slice
 
-const nextEl = document.querySelector(".next");
+// const nextEl = document.querySelector(".next");
 
-const prevEl = document.querySelector(".prev");
+// const prevEl = document.querySelector(".prev");
 
-const imgsEl = document.querySelectorAll(".image_slider");
+// const imgsEl = document.querySelectorAll(".image_slider");
 
-const imageContainerEl = document.querySelector(".image-container");
+// const imageContainerEl = document.querySelector(".image-container");
 
-let currentImg = 1;
+// let currentImg = 1;
 
-let timeout;
+// let timeout;
 
-nextEl.addEventListener("click", () => {
-  currentImg++;
-  clearTimeout(timeout);
-  updateImg();
+// nextEl.addEventListener("click", () => {
+//   currentImg++;
+//   clearTimeout(timeout);
+//   updateImg();
+// });
+
+// prevEl.addEventListener("click", () => {
+//   currentImg--;
+//   clearTimeout(timeout);
+//   updateImg();
+// });
+
+// updateImg();
+
+// function updateImg() {
+//   if (window.screen.availWidth >= 1024){
+//     if (currentImg > imgsEl.length) {
+//       currentImg = 1;
+//     } else if (currentImg < 1) {
+//       currentImg = imgsEl.length;
+//     }
+//     prevEl.style.transform=`translateX(-1024px)`;
+//     nextEl.style.transform=`translateX(0)`;
+//     // imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 1024}px)`;
+//     timeout = setTimeout(() => {
+//       currentImg++;
+//       updateImg();
+//     }, 3000);}
+//   else if (window.screen.availWidth >= 760){
+//     if (currentImg > imgsEl.length) {
+//       currentImg = 1;
+//     } else if (currentImg < 1) {
+//       currentImg = imgsEl.length;
+//     }
+//     imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 768}px)`;
+//     timeout = setTimeout(() => {
+//       currentImg++;
+//       updateImg();
+//     }, 3000);
+//   }else{
+//     if (currentImg > imgsEl.length) {
+//       currentImg = 1;
+//     } else if (currentImg < 1) {
+//       currentImg = imgsEl.length;
+//     }
+//     imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 300}px)`;
+//     timeout = setTimeout(() => {
+//       currentImg++;
+//       updateImg();
+//     }, 3000);
+//   }
+// }
+
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
-
-prevEl.addEventListener("click", () => {
-  currentImg--;
-  clearTimeout(timeout);
-  updateImg();
-});
-
-updateImg();
-
-function updateImg() {
-  if (window.screen.availWidth >= 1024){
-    if (currentImg > imgsEl.length) {
-      currentImg = 1;
-    } else if (currentImg < 1) {
-      currentImg = imgsEl.length;
-    }
-    imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 1024}px)`;
-    timeout = setTimeout(() => {
-      currentImg++;
-      updateImg();
-    }, 3000);}
-  else if (window.screen.availWidth >= 760){
-    if (currentImg > imgsEl.length) {
-      currentImg = 1;
-    } else if (currentImg < 1) {
-      currentImg = imgsEl.length;
-    }
-    imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 768}px)`;
-    timeout = setTimeout(() => {
-      currentImg++;
-      updateImg();
-    }, 3000);
-  }else{
-    if (currentImg > imgsEl.length) {
-      currentImg = 1;
-    } else if (currentImg < 1) {
-      currentImg = imgsEl.length;
-    }
-    imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 300}px)`;
-    timeout = setTimeout(() => {
-      currentImg++;
-      updateImg();
-    }, 3000);
-  }
-}
-
-
-
 
 
 
