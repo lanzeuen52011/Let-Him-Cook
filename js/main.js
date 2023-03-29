e13303a9dfbf7943efe1143f832ba4ddbb13d55b
ZiYun Lan <lanzeuen52011@gmail.com>
2023-03-29 20:57:30 +0800

    swiper auto play

--- js/main.js
+++ js/main.js
@@ -82,6 +82,10 @@ const swiper = new Swiper('.swiper', {
   pagination: {
     el: '.swiper-pagination',
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 
   // Navigation arrows
   navigation: {
